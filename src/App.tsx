64c8bb64cc60d89c4a6daa5b060994dbcb557139
Dijango Alves <dijango_ilha@hotmail.com>
2020-09-02 13:14:07 -0300

    fix: resolve bug no-unused-vars at import React dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import React from 'react';
+import React from 'react'
 
-function App() {
+const App: React.FC = () => {
   return (
     <div>
       App
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
