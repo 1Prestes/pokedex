9d2cfff159ece07c5efe8489ff5f795157f71514
Dijango Alves <dijango_ilha@hotmail.com>
2020-09-17 14:32:45 -0300

    Updates

---
+++ precache-manifest.fb1ad3a489fd747d26677ee8951eaa69.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f56f49b2d21852557a4d3058ff17f2e7",
+    "url": "/pokedex/index.html"
+  },
+  {
+    "revision": "7618a840428171bcd2da",
+    "url": "/pokedex/static/css/main.008a394a.chunk.css"
+  },
+  {
+    "revision": "0a1f6fb3b4ebc2cf26f5",
+    "url": "/pokedex/static/js/2.6e67f71c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pokedex/static/js/2.6e67f71c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7618a840428171bcd2da",
+    "url": "/pokedex/static/js/main.cca5590e.chunk.js"
+  },
+  {
+    "revision": "5d5b7317cf43a0b8925c",
+    "url": "/pokedex/static/js/runtime-main.2d94522c.js"
+  },
+  {
+    "revision": "442867e6204969c4bef95be4435ab54e",
+    "url": "/pokedex/static/media/search.442867e6.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e174d7fd9c14277dd593ad7bad85f5fa.js"
+  "/pokedex/precache-manifest.fb1ad3a489fd747d26677ee8951eaa69.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/pokedex/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
